f63c8ad2cf054b048808448cf064b3354ee073a3
Jorge/Deepak <deepakkumar.si@hcl.com>
2018-07-17 12:02:48 -0600

    Added refresh scope to registration server

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,6 +21,7 @@ import java.util.TimeZone;
 @EnableCircuitBreaker
 @EnableWebSecurity
 @EnableResourceServer
+@RefreshScope
 public class App {
 
     public static void main(String[] args) {
