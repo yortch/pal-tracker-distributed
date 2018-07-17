dc5e4a0219ef73f9f7d5a0a41505bc041e000051
Jorge/Deepak <deepakkumar.si@hcl.com>
2018-07-17 14:07:03 -0600

    Updated App files to have refresh scope

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -2,6 +2,7 @@ package io.pivotal.pal.tracker.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,6 +22,7 @@ import java.util.TimeZone;
 @EnableWebSecurity
 @EnableResourceServer
 @EnableEurekaClient
+@RefreshScope
 public class App {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
